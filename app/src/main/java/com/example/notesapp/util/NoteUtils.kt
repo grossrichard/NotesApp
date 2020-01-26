9036f01b1ee069e9fb39c7923e04e231bbad2864
Richard Gross <grossrichard555@gmail.com>
2020-01-27 00:24:43 +0100

    cleared code

--- app/src/main/java/com/example/notesapp/util/NoteUtils.kt
+++ app/src/main/java/com/example/notesapp/util/NoteUtils.kt
@@ -1,6 +1,5 @@
 package com.example.notesapp.util
 
-import androidx.annotation.MenuRes
 import androidx.annotation.StringRes
 import com.example.notesapp.R
 import com.example.notesapp.entity.NoteDetailMode
@@ -20,21 +19,4 @@ object NoteUtils {
             CREATE -> R.string.note_detail_title_create
             DELETE -> R.string.note_detail_title_delete
         }
-
-    @JvmStatic
-    fun isEditable(mode: NoteDetailMode): Boolean =
-        when (mode) {
-            READ, DELETE -> false
-            EDIT, CREATE -> true
-        }
-
-//    @JvmStatic
-//    @MenuRes
-//    fun getNoteDetailMenu(mode: NoteDetailMode): Int =
-//        when (mode) {
-//            READ -> R.menu.menu_detail
-//            EDIT -> R.menu.menu_detail_edit
-//            CREATE -> R.menu.menu_confirm
-//            DELETE -> R.menu.menu_detail_delete
-//        }
 }
